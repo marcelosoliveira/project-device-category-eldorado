f217afe169023cbd7ed214b82eb05fb24748d5fd
marcelosoliveira <msbobsk8@gmail.com>
2021-08-27 12:52:08 -0300

    Foi atualizado o arquivo .env e contruido o arquivo de inicialização da api rest

---
+++ back-end/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const status = require('http-status-codes');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('./swagger.json');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(
+  '/swagger.html-ui',
+  swaggerUi.serve, 
+  swaggerUi.setup(swaggerDocument)
+);
+
+const { PORT } = process.env;
+
+
+app.listen(PORT, () => {
+    console.log(`App executando na porta ${ PORT }`);
+});
+
+app.use((err, _req, res, _next) => {
+    res.status(status.INTERNAL_SERVER_ERROR).json({
+        Error: `ERROR! ${err.message}`,
+    })
+});
