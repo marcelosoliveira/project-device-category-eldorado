855fbc6d5887a620b2c5a97e0bb267cf9f7abf6a
marcelosoliveira <msbobsk8@gmail.com>
2021-08-27 13:31:29 -0300

    Foi criado as tabelas do banco de dados device_category

---
+++ back-end/db/device_category.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE IF NOT EXISTS device_category;
+
+CREATE TABLE IF NOT EXISTS category (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(128) NOT NULL
+) ENGINE InnoDB DEFAULT CHARSET='utf8';
+
+CREATE TABLE IF NOT EXISTS device (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    category VARCHAR(128) NOT NULL,
+    cor VARCHAR(16) NOT NULL,
+    part_number INTEGER NOT NULL,
+    id_category INTEGER NOT NULL,
+    FOREIGN KEY (id_category) REFERENCES category(id)
+) ENGINE InnoDB DEFAULT CHARSET='utf8';
+
+INSERT INTO category(name) VALUES('Eletronicos');
+INSERT INTO category(name) VALUES('Serviços');
+INSERT INTO category(name) VALUES('Mecânica');
+INSERT INTO category(name) VALUES('Cozinha');
+INSERT INTO category(name) VALUES('Automotivos');
+
+INSERT INTO device(category, cor, part_number, id_category)
+VALUES('Eletronicos', 'Azul', 1111, 1);
+INSERT INTO device(category, cor, part_number, id_category)
+VALUES('Automotivos', 'Verde', 1121, 5);
+INSERT INTO device(category, cor, part_number, id_category)
+VALUES('Mecânica', 'Preto', 1231, 3);
+INSERT INTO device(category, cor, part_number, id_category)
+VALUES('Serviços', 'Marrom', 1431, 2);
+INSERT INTO device(category, cor, part_number, id_category)
+VALUES('Cozinha', 'Vermelho', 1141, 4);
