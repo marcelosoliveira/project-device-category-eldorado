d1cf5c187933b811a9b4c12f259c8e952808216a
marcelosoliveira <msbobsk8@gmail.com>
2021-08-27 14:48:14 -0300

    Foi implemetado o relacionamento entre dispositivos e categorias

--- back-end/src/model/Device.js
+++ back-end/src/model/Device.js
@@ -1,7 +1,9 @@
 const conn = require('../../config/connect')
 
 const listAllDevices = async () => {
-    const [devices] = await conn.execute('SELECT * FROM device');
+    const [devices] = await conn.execute(`SELECT d.id AS Id, d.name AS Name,
+     d.cor AS Cor, d.part_number AS PartNumber, c.name AS Category FROM device AS d 
+    INNER JOIN category AS c ON c.id = d.id_category`);
 
     return devices;
 }
