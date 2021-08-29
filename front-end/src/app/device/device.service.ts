fbe3e0d2d898baa7e85aa9b8c4fce9f20700ce99
marcelosoliveira <msbobsk8@gmail.com>
2021-08-29 17:17:11 -0300

    Implementado o arquivo device service e od componentes de lista e criacao

---
+++ front-end/src/app/device/device.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Device } from "./device";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DeviceService {
+
+    private baseUrl = 'http://localhost:3000/api/v1/device';
+
+    constructor(private httpClient: HttpClient) {};
+
+    public retrieveAll(): Observable<Device[]> {
+        return this.httpClient.get<Device[]>(this.baseUrl);
+    }
+
+    public retrieveById(id: number): Observable<Device> {
+        return this.httpClient.get<Device>(`${this.baseUrl}/${id}`);
+    }
+
+    public save(device: Device): Observable<Device> {
+        console.log(device);
+        const object = { name: device.name, color: device.color,
+            part_number: device.part_number, id_category: Number(device.id_category) };
+        return this.httpClient.post<Device>(`${this.baseUrl}/create`, object);
+    }
+
+    public deleteById(id: number): Observable<any> {
+        return this.httpClient.delete<any>(`${this.baseUrl}/delete/${id}`);
+    }
+   
+}
