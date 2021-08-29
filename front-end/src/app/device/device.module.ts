05da161e81d3fa74b4a2aac1321ff7782b55565e
marcelosoliveira <msbobsk8@gmail.com>
2021-08-29 17:12:05 -0300

    Foi implementado as rotas do device

---
+++ front-end/src/app/device/device.module.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { DeviceCreateComponent } from "./device-create.component";
+import { DeviceListComponent } from "./device-list.component";
+
+@NgModule({
+    declarations: [
+        DeviceListComponent,
+        DeviceCreateComponent,
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild([
+            {
+                path: 'devices',
+                component: DeviceListComponent,
+            },
+            {
+                path: 'device/create',
+                component: DeviceCreateComponent,
+            },
+        ]),
+    ]    
+})
+export class DeviceModule { }
