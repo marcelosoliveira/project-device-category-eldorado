2d0b0d75f11fa4658d67520afc5a250720df6be2
marcelosoliveira <msbobsk8@gmail.com>
2021-08-29 16:59:53 -0300

    Foi criado o model do device

---
+++ front-end/src/app/device/device.ts
@@ -0,0 +1,10 @@
+export class Device { 
+
+    public id!: number;
+    public name!: string;
+    public color!: string;
+    public part_number!: number;
+    public id_category!: number;
+    public category!: string;
+    public message!: string;
+}
